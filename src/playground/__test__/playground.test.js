f8bb464bc40f5195b65907be9fbf5beff5260f10
znnnnn <846179626@qq.com>
2019-09-16 01:55:14 +0800

    add: getBit

--- src/playground/__test__/playground.test.js
+++ src/playground/__test__/playground.test.js
@@ -1,5 +1,10 @@
+import getBit from '../playground'
+
 describe('playground', () => {
   it('should perform playground tasks', () => {
     // Place your playground tests here.
-  });
-});
+    // 10 = 0b1010
+    expect(getBit(10, 0)).toBe(0)
+    expect(getBit(10, 5)).toBe(0)
+  })
+})
--- src/playground/playground.js
+++ src/playground/playground.js
@@ -1,1 +1,9 @@
 // Place your playground code here.
+/**
+ * @param {number} number
+ * @param {number} bitPosition - zero based.
+ * @return {number}
+ */
+export default function getBit(number, bitPosition = 0) {
+  return (number >> bitPosition) & 1;
+}
