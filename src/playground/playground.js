871a7a499a0c391348f4ebb12c62464142fa4092
znnnnn <846179626@qq.com>
2019-09-19 20:59:57 +0800

    add: bitDiff

--- src/playground/playground.js
+++ src/playground/playground.js
@@ -3,12 +3,6 @@
  * @param {number} number
  * @return {number}
  */
-export default function countSitBit(originNumber) {
-  let counter = 0
-  let number = originNumber
-  while (number) {
-    counter += number & 1
-    number >>= 1
-  }
-  return counter
+export default function xor(numberA, numberB) {
+  return numberA ^ numberB
 }
