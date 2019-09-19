465ffa922bdcc9f8ca4522dfe6f46e3163bcc6b1
znnnnn <846179626@qq.com>
2019-09-19 21:17:44 +0800

    add: isPowerOfTwo

--- src/playground/__test__/playground.test.js
+++ src/playground/__test__/playground.test.js
@@ -2,6 +2,7 @@ import test from '../playground';
 
 describe('playground', () => {
   it('should perform playground tasks', () => {
-    expect(test(8)).toBe(4);
+    expect(test(8)).toBe(true);
+    expect(test(9)).toBe(false);
   });
 });
--- src/playground/playground.js
+++ src/playground/playground.js
@@ -3,10 +3,6 @@
  * @param {number} number
  * @return {number}
  */
-export default function bitLength(number) {
-  let bitLenCount = 0
-  while ((1 << bitLenCount) <= number) {
-    bitLenCount += 1
-  }
-  return bitLenCount
+export default function isPowerOfTwo(number) {
+  return (number & (number - 1)) === 0
 }
