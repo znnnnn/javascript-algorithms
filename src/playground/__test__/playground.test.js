319522cd37ba4f165ef8254a141539542e06147a
znnnnn <846179626@qq.com>
2019-09-28 15:21:53 +0800

    add: 素数筛

--- src/playground/__test__/playground.test.js
+++ src/playground/__test__/playground.test.js
@@ -2,6 +2,9 @@ import test from '../playground';
 
 describe('playground', () => {
   it('should perform playground tasks', () => {
-    expect(test(20, 12)).toBe(4);
+    expect(test(100)).toEqual([
+      2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+      43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+    ]);
   });
 });
--- src/playground/playground.js
+++ src/playground/playground.js
@@ -3,8 +3,16 @@
  * @param {number} number
  * @return {number}
  */
-export default function euclideanAligorithm(numberA, numberB) {
-  const a = Math.abs(numberA)
-  const b = Math.abs(numberB)
-  return b === 0 ? a : euclideanAligorithm(b, a % b)
+export default function sieveOfEratosthenes(n) {
+  const notPrimes = new Uint8Array(n + 1)
+  const primes = []
+  for (let i = 2; i <= n; i += 1) {
+    if (notPrimes[i] === 0) {
+      primes.push(i)
+      for (let j = 1; j * i <= n; j += 1) {
+        notPrimes[i * j] = true
+      }
+    }
+  }
+  return primes
 }
